8e5c361d9054bba38634e9ceb1c545149cb3d8b6
naaltunian <naaltunian@icloud.com>
2018-10-15 22:07:54 -0400

    style app

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Track = (props) => {
     const { track } = props;
@@ -9,9 +10,12 @@ const Track = (props) => {
                 <h5>{track.artist_name}</h5>
                 <p className="card-text">
                     <strong><i className="fas fa-play"></i>Track</strong>: {track.track_name}
+                    <br />
                     <strong><i className="fas fa-compact-disc"></i>Album</strong>: {track.album_name}
                 </p>
-                <br />
+                <Link to={`lyrics/tracks/${track.track_id}`} className="btn btn-dark btn-block" >
+                    <i className="fas fa-chevron-right"></i> View Lyrics
+                </Link>
             </div>
         </div>
       </div>
